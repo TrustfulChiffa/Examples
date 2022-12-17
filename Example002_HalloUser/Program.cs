c6f155df891de3b04f2e0c021d030c8b7728858d
Chiffa <babiy99021422@gmail.com>
2022-12-17 23:31:13 +0300

    приветствие пользователя

---
+++ Example002_HalloUser/Program.cs
@@ -0,0 +1,8 @@
+﻿// Console.WriteLine - Вывод в терминал и перевод курсора на новую строку с новой строки 
+// Console.WriteLine("Введите ваше имя "); 
+Console.Write("Введите ваше имя "); // Write - вывод в терминал и оставление крсора на той же строке
+string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+Console.Write("Привет, ");
+// Console.WriteLine(username);
+Console.Write(username);

db8eb517c77c00d6d0b184ee37e7a63184cc62c3
Chiffa <babiy99021422@gmail.com>
2022-12-18 16:21:22 +0300

    Add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,11 @@
-﻿int a =3;
-int b =5;
-int result= a+b;
+﻿int a = new Random().Next(1, 10); // Случайное число от 1 до 9
+Console.WriteLine(a);
+
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+
+// int a = 3;
+// int b = 5;
+int result = a + b;
 // Console.WriteLine(a+b);
 Console.WriteLine(result);
